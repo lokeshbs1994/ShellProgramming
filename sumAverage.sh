d3aec3e8bbdad11a66587829c5b54a7ee8b974da
lokeshbs1994 <lokeshbs619@gmail.com>
2022-01-14 15:48:50 +0530

    basic

--- sumAverage.sh
+++ sumAverage.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -x
 
-N=5;
 var1=$1
 var2=$2
 var3=$3
@@ -8,5 +7,6 @@ var4=$4
 var5=$5
 
 sum=$(( $var1 + $var2 + $var3 + $var4 + $var5 ))
-echo $sum
-average=`expr $sum / $N `
+echo "Sum=$sum"
+average=$(( $sum / 5 ))
+echo "Average=$average"
