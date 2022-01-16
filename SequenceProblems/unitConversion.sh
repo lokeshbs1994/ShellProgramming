71c4d260d683f0a531daa2eb1ed36c36f3b63302
lokeshbs1994 <lokeshbs619@gmail.com>
2022-01-16 16:31:18 +0530

    Added: Unit conversion problem fully solved

--- SequenceProblems/unitConversion.sh
+++ SequenceProblems/unitConversion.sh
@@ -1,13 +1,18 @@
-#!/bin/bash -x
+#!/bin/bash
 
 echo "a. 1ft = 12 inches then 42 inches = ? ft"
-echo "42inches = $((42 / 12)) ft"
+length=42
+lengthInMeters=`echo "$length" | awk '{a=$1*0.0833; print a}'` 
+echo "42 inches =$lengthInMeters meters"
 
 echo "b. Rectangular Plot of 60 feet x 40 feet in meters"
-var1= $(($((60*40)) * ((3/10)) ))
-echo "60 feet x 40 feet = $var1 meters"
+Length=`echo 60 | awk '{a=$1*0.3048; print a}'`
+Width=`echo 40 | awk '{a=$1*0.3048; print a}'`
+Area=`echo "$Length $Width" | awk '{area=$1*$2; print area}'`
+echo "Area =$Area sq meters"
 
 echo "c. Calculate area of 25such polts in acre"
-var2= $(($var1 * 25))
-echo "area in meter^2 =$var2"
+echo "So 25 X $Area sq meters in acres is "
+totalArea=`echo "$Area" | awk '{a=$1*25*0.0002471; print a}'`
+echo "Total Area =$totalArea acres"
 
